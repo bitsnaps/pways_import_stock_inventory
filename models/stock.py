253e5585eb2b4a83bfd6aeb30447bad080e68a5c
Ibrahim.H <bitsnaps@yahoo.fr>
2022-08-25 01:59:36 +0100

    update master branch to v14

--- models/stock.py
+++ models/stock.py
@@ -20,6 +20,16 @@ try:
 except ImportError:
     _logger.debug('Cannot `import xlrd`.')
 
+# class StockInventory(models.Model):
+#     _inherit = 'stock.inventory'
+
+#     def action_start(self):
+#         for inventory in self.filtered(lambda x: x.state not in ('done','cancel')):
+#             vals = {'state': 'confirm', 'date': inventory.date or fields.Datetime.now()}
+#             if (inventory.filter != 'partial') and not inventory.line_ids:
+#                 vals.update({'line_ids': [(0, 0, line_values) for line_values in inventory._get_inventory_lines_values()]})
+#             inventory.write(vals)
+#         return True
 
 class ImportStockInventory(models.TransientModel):
     _name = "import.stock.inventory"
@@ -33,8 +43,7 @@ class ImportStockInventory(models.TransientModel):
     is_validate_inventory = fields.Boolean(string="Validate Inventory")
     date = fields.Datetime(string='Inventory Date', default=lambda self: fields.datetime.now() , required=True)
 
-    @api.multi
-    def import_xls(self):
+    def import_csv(self):
         if self.import_option == 'xls':
             try:
                 fp = tempfile.NamedTemporaryFile(delete= False,suffix=".xlsx")
@@ -60,11 +69,12 @@ class ImportStockInventory(models.TransientModel):
                 if not product_id:
                     raise exceptions.Warning(_("Product %s does not exist !" % line.get('Product')))
                 if lot:
-                    prod_lot_id = self.env['stock.production.lot'].sudo().create({'name': lot, 'product_id': product_id.id})
+                    prod_lot_id = self.env['stock.production.lot'].sudo().create({'name': lot, 'product_id': product_id.id, 'company_id': self.env.user.company_id.id})
                     if not prod_lot_id:
                         raise exceptions.Warning(_("Lot %s does not exist !" % line.get('Serial')))
                 if not inventory_obj:
-                    inventory_obj = inventory_obj.create({'name':self.inv_name,'filter':'partial','location_id':self.location_id.id, 'date': self.date})
+                    inventory_obj = inventory_obj.create({'name':self.inv_name,'location_ids':self.location_id.ids, 'date': self.date})
+                    # inventory_obj.action_start()
                 if inventory_obj:
                     stock_line_id = self.env['stock.inventory.line'].sudo().create({
                         'product_id': product_id.id,
